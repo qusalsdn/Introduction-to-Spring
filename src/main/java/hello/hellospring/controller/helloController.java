adb9be66d411a7987c57298c22d895033e4f2589
변민우 <yja05092@gmail.com>
2024-03-16 15:54:16 +0900

    API
    1. @ResponseBody를 이용하면 응답 바디부에 내용을 직접 넣어 응답할 수 있다.
    2. getter setter를 이용하여 객체를 key & value 형태로 변환하고 JSON을 반환한다.

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class helloController {
@@ -17,5 +22,42 @@ public class helloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        List<Integer> values = Arrays.asList(1, 2, 3);
+        hello.setTest(values);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+        private List<Integer> test;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Integer> getTest() {
+            return test;
+        }
+
+        public void setTest(List<Integer> test) {
+            this.test = test;
+        }
     }
 }
